27644dba608393511cf0a70b6c92c9b8b432eb42
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-19 16:18:05 +0200

    form items

--- accountform/ContentView.swift
+++ accountform/ContentView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName = "";
+    @State private var lastName = "";
+    @State private var birthDate = Date();
+    @State private var isSendNewsletter = false;
+    @State private var likesNumber = 0;
+    
+    
     var body: some View {
-        Text("Hello, world!!")
-            .padding()
+        NavigationView {
+            
+            Form{
+                Section(header: Text("Personal Information" )) {
+                    TextField("First Name" , text: $firstName)
+                    TextField("Last Name" , text: $lastName)
+                    DatePicker("Birthday" , selection: $birthDate , displayedComponents: .date)
+                }
+                Section(header: Text("Actions")) {
+                    Toggle("Send Newsletter" , isOn: $isSendNewsletter)
+                        .toggleStyle(SwitchToggleStyle(tint: .red))
+                    Stepper("Number of Likes", value: $likesNumber , in: 1...100)
+                    Text("This video has \(likesNumber) likes")
+                    Link("Terms of service", destination: URL(string: "https://google.com")!)
+                    
+                }
+            
+            }
+            .accentColor(.green)
+            .navigationTitle("Account")
+        }
     }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
