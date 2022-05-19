74c5633704625dceb4d7fbbefc3db5929779318a
“Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-05-19 10:51:03 +0200

    Initial Commit

---
+++ accountform/accountformApp.swift
@@ -0,0 +1,17 @@
+//
+//  accountformApp.swift
+//  accountform
+//
+//  Created by Cloud Secrets on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct accountformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
